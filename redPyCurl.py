061d85e99ebadbd2b0540361fe56e22c8b718c20
rushton <stephen.garth@gmail.com>
2016-05-05 12:57:58 -0500

    Removal of CrytoPickle Dependancy

--- redPyCurl.py
+++ redPyCurl.py
@@ -1,19 +1,20 @@
 #!/usr/env python
 
 
-#import pycurl,json
+import pycurl
 import os,json
 from cStringIO import StringIO
 from operator import itemgetter
 from fetcher import CurlWorker
-from capper import CapProj
 
 
-class redcurl(CurlWorker,CapProj):
-	def __init__(self,proj,cappage,usercrypt=None,keyloc=None):
+
+
+class RedCurl(CurlWorker):
+	def __init__(self,cappage,token=''):
 		CurlWorker.__init__(self,cappage)
-		CapProj.__init__(self,proj,loc=keyloc,usercrypt=usercrypt)
 		self.output = ""
+		self.t = token
 	def _inputhandler(self,incoming):
 		if isinstance(incoming,list):
 			return ','.join(str(i) for i in incoming)
@@ -21,26 +22,42 @@ class redcurl(CurlWorker,CapProj):
 			return incoming if isinstance(incoming,str) else str(incoming)
 		else:
 			raise TypeError("Input must be str or list\n\tRecieved: %s" %(str(type(incoming))))
-	def metagrab(self,style='json'):
-		dat = [('token',self.tokretr()),('content','metadata'),('format',style)]
+	def gettoken(self): 
+		return self.t
+	def settoken(self, token):
+		self.t = token
+	def deltoken(self):
+		del self.t
+	token = property(gettoken, settoken, deltoken, "Token property")
+
+	def metagrab(self,style='json',token=''):
+		if token:
+			self.settoken(token)
+		dat = [('token',self.t),('content','metadata'),('format',style)]
 		self.loader(dat)
 		self.output = self.grab()
 		if style == 'json':
 			return json.loads(self.output)
 		return self.output
 
-	def fieldnames(self):
+	def fieldnames(self,style='json',token=''):
 		return map(itemgetter('field_name'),self.metagrab('json'))
-	def pullform(self,recno,forms,style='json'):
-		dat = [('token',self.tokretr()),('content','record'),('format',style),('action','export')]
+	def pullform(self,recno,forms,style='json',token=''):
+		if not self.t:
+			self.t = self.gettoken()
+
+		dat = [('token',self.t),('content','record'),('format',style),('action','export')]
 		recno = self._inputhandler(recno)
 		topull = self._inputhandler(forms)
 		dat.extend([('records',recno),('forms',topull)])
 		self.loader(dat)
 		self.output = self.grab()
 		return json.loads(self.output) if style == 'json' else self.output
-	def pullrec(self,recno,style='json',fields=None):
-		dat = [('token',self.tokretr()),('content','record'),('format',style),('action','export')]
+	def pullrec(self,recno,style='json',fields=None,token=''):
+		if not self.t:
+			self.t = self.gettoken()
+
+		dat = [('token',self.t),('content','record'),('format',style),('action','export')]
 		recno = self._inputhandler(recno)
 		dat.append(('records',recno))
 		if fields:
@@ -51,17 +68,23 @@ class redcurl(CurlWorker,CapProj):
 		if style == 'json':
 			return json.loads(self.output)
 		return self.output
-	def pushrec(self,recno,**kwargs):
+	def pushrec(self,recno,token='',**kwargs):
+		if not self.t:
+			self.t = self.gettoken()
+
 		data = {'participant_id':recno}
 		data.update(kwargs)
 		payload = [('content','record')]
 		datain = json.dumps([data])
-		payload.extend([('type','flat'),('format','json'),('token',self.tokretr()),('data',datain)])
+		payload.extend([('type','flat'),('format','json'),('token',self.t),('data',datain)])
 		self.loader(payload)
 		self.output = self.grab()
 		return self.output
 
-	def fullpull(self,delim=',',recs=[],fields=[]):
+	def fullpull(self,delim=',',recs=[],fields=[],token=''):
+		if not self.t:
+			self.t = self.gettoken()
+
 		import csv
 		if len(fields):
 			if isinstance(fields[0],tuple):
@@ -81,10 +104,12 @@ class redcurl(CurlWorker,CapProj):
 		buff.close()
 		return out
 
-	def pushfile(self,recno,fieldname,localname,rcname,pid=None):
+	def pushfile(self,recno,fieldname,localname,rcname,pid=None,token=''):
+		if not self.t:
+			self.t = self.gettoken()
 		dat = [('content','file'),('action','import'),('record',recno),('field',fieldname)]
 		dat.extend([('file', (pycurl.FORM_FILE, localname, pycurl.FORM_FILENAME, rcname))])
-		dat.extend([('token',self.tokretr())])
+		dat.extend([('token',self.t)])
 		if pid:
 			dat.extend([('project_id',str(pid))])
 		self.loader(dat)
